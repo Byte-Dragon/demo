b80bf7c92ddfad51e5e32e3d65aaf35d24714613
Byte-Dragon <ljl2715933092@foxmail.com>
2025-06-08 23:21:19 +0800

    Revert "test commit-test-0608"
    This reverts commit 37742f156f02a0632342af4c994a3e4124be19eb.

--- demo/src/main/java/com/example/demo/ServletInitializer.java
+++ demo/src/main/java/com/example/demo/ServletInitializer.java
@@ -6,9 +6,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
-    {
-        // 注释
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(DemoApplication.class);
     }
 
